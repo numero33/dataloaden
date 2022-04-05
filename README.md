268ef6d8338e2c53616ccab5822468e9b0e2dfaf
PM <12273891+n33pm@users.noreply.github.com>
2022-04-05 14:25:59 +0200

    init(): Bazinga

---
+++ README.md
@@ -0,0 +1,49 @@
+# dataloaden
+![GitHub Workflow Status](https://img.shields.io/github/workflow/status/numero33/dataloaden/Test)
+[![Go Report Card](https://goreportcard.com/badge/github.com/numero33/dataloaden)](https://goreportcard.com/report/github.com/numero33/dataloaden)
+[![Coverage Status](https://coveralls.io/repos/github/numero33/dataloaden/badge.svg)](https://coveralls.io/github/numero33/dataloaden)
+
+> This is an implementation of [Facebook's DataLoader](https://github.com/facebook/dataloader) in **Golang 1.18**.
+
+Heavily inspired by [`vektah/dataloaden`](https://github.com/vektah/dataloaden)
+
+## Install
+
+```bash
+go get -u github.com/numero33/dataloaden
+```
+
+## Usage
+
+```golang
+
+type User struct {
+	ID   string
+	Name string
+}
+
+// NewLoader will collect user requests for 2 milliseconds and send them as a single batch to the fetch func
+// normally fetch would be a database call.
+func NewLoader() *dataloaden.Loader[string, *User] {
+	return dataloaden.NewLoader(dataloaden.LoaderConfig[string, *User]{
+		Wait:     2 * time.Millisecond,
+		MaxBatch: 100,
+		Fetch: func(keys []string) ([]*User, []error) {
+			users := make([]*User, len(keys))
+			errors := make([]error, len(keys))
+
+			for i, key := range keys {
+				users[i] = &User{ID: key, Name: "user " + key}
+			}
+			return users, errors
+		},
+	})
+}
+
+func main() {
+    userLoader := NewLoader()
+    u, _ := dl.Load("E1")
+}
+
+
+```
---
+++ dataloaden_test.go
@@ -0,0 +1,186 @@
+package dataloaden
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+// User is some kind of database backed model
+type User struct {
+	ID   string
+	Name string
+}
+
+func TestLoader(t *testing.T) {
+	var fetches [][]string
+	var mu sync.Mutex
+
+	dl := NewLoader(LoaderConfig[string, *User]{
+		Wait:     10 * time.Millisecond,
+		MaxBatch: 5,
+		Fetch: func(keys []string) ([]*User, []error) {
+			mu.Lock()
+			fetches = append(fetches, keys)
+			mu.Unlock()
+
+			users := make([]*User, len(keys))
+			errors := make([]error, len(keys))
+
+			for i, key := range keys {
+				if strings.HasPrefix(key, "E") {
+					errors[i] = fmt.Errorf("user not found")
+				} else {
+					users[i] = &User{ID: key, Name: "user " + key}
+				}
+			}
+			return users, errors
+		},
+	})
+
+	t.Run("fetch concurrent data", func(t *testing.T) {
+		t.Run("load user successfully", func(t *testing.T) {
+			t.Parallel()
+			u, err := dl.Load("U1")
+			require.NoError(t, err)
+			require.Equal(t, u.ID, "U1")
+		})
+
+		t.Run("load failed user", func(t *testing.T) {
+			t.Parallel()
+			u, err := dl.Load("E1")
+			require.Error(t, err)
+			require.Nil(t, u)
+		})
+
+		t.Run("load many users", func(t *testing.T) {
+			t.Parallel()
+			u, err := dl.LoadAll([]string{"U2", "E2", "E3", "U4"})
+			require.Equal(t, u[0].Name, "user U2")
+			require.Equal(t, u[3].Name, "user U4")
+			require.Error(t, err[1])
+			require.Error(t, err[2])
+		})
+
+		t.Run("load thunk", func(t *testing.T) {
+			t.Parallel()
+			thunk1 := dl.LoadThunk("U5")
+			thunk2 := dl.LoadThunk("E5")
+
+			u1, err1 := thunk1()
+			require.NoError(t, err1)
+			require.Equal(t, "user U5", u1.Name)
+
+			u2, err2 := thunk2()
+			require.Error(t, err2)
+			require.Nil(t, u2)
+		})
+	})
+
+	t.Run("fetch more", func(t *testing.T) {
+
+		t.Run("previously cached", func(t *testing.T) {
+			t.Parallel()
+			u, err := dl.Load("U1")
+			require.NoError(t, err)
+			require.Equal(t, u.ID, "U1")
+		})
+
+		t.Run("load many users", func(t *testing.T) {
+			t.Parallel()
+			u, err := dl.LoadAll([]string{"U2", "U4"})
+			require.NoError(t, err[0])
+			require.NoError(t, err[1])
+			require.Equal(t, u[0].Name, "user U2")
+			require.Equal(t, u[1].Name, "user U4")
+		})
+	})
+
+	t.Run("no round trips", func(t *testing.T) {
+		mu.Lock()
+		defer mu.Unlock()
+
+		require.Len(t, fetches, 2)
+	})
+
+	t.Run("fetch partial", func(t *testing.T) {
+		t.Run("errors not in cache cache value", func(t *testing.T) {
+			t.Parallel()
+			u, err := dl.Load("E2")
+			require.Nil(t, u)
+			require.Error(t, err)
+		})
+
+		t.Run("load all", func(t *testing.T) {
+			t.Parallel()
+			u, err := dl.LoadAll([]string{"U1", "U4", "E1", "U9", "U5"})
+			require.Equal(t, u[0].ID, "U1")
+			require.Equal(t, u[1].ID, "U4")
+			require.Error(t, err[2])
+			require.Equal(t, u[3].ID, "U9")
+			require.Equal(t, u[4].ID, "U5")
+		})
+	})
+
+	t.Run("primed reads dont hit the fetcher", func(t *testing.T) {
+		dl.Prime("U99", &User{ID: "U99", Name: "Primed user"})
+		u, err := dl.Load("U99")
+		require.NoError(t, err)
+		require.Equal(t, "Primed user", u.Name)
+
+		require.Len(t, fetches, 3)
+	})
+
+	t.Run("priming in a loop is safe", func(t *testing.T) {
+		users := []User{
+			{ID: "Alpha", Name: "Alpha"},
+			{ID: "Omega", Name: "Omega"},
+		}
+		for _, user := range users {
+			u := &user
+			cpy := *u
+			dl.Prime(user.ID, &cpy)
+		}
+
+		u, err := dl.Load("Alpha")
+		require.NoError(t, err)
+		require.Equal(t, "Alpha", u.Name)
+
+		u, err = dl.Load("Omega")
+		require.NoError(t, err)
+		require.Equal(t, "Omega", u.Name)
+
+		require.Len(t, fetches, 3)
+	})
+
+	t.Run("cleared results will go back to the fetcher", func(t *testing.T) {
+		dl.Clear("U99")
+		u, err := dl.Load("U99")
+		require.NoError(t, err)
+		require.Equal(t, "user U99", u.Name)
+
+		require.Len(t, fetches, 4)
+	})
+
+	t.Run("load all thunk", func(t *testing.T) {
+		thunk1 := dl.LoadAllThunk([]string{"U5", "U6"})
+		thunk2 := dl.LoadAllThunk([]string{"U6", "E6"})
+
+		users1, err1 := thunk1()
+
+		require.NoError(t, err1[0])
+		require.NoError(t, err1[1])
+		require.Equal(t, "user U5", users1[0].Name)
+		require.Equal(t, "user U6", users1[1].Name)
+
+		users2, err2 := thunk2()
+
+		require.NoError(t, err2[0])
+		require.Error(t, err2[1])
+		require.Equal(t, "user U6", users2[0].Name)
+	})
+}
